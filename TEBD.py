73bd3039612b83f4c66b3929c792bc894e323672
Sazbo7 <jszabo94@gmail.com>
2021-03-11 15:17:26 -0500

    edit

--- TEBD.py
+++ TEBD.py
@@ -11,6 +11,9 @@ import scipy.integrate
 
 
 def TEBD_mixed_ising_confined(L, h, g, tmax, dt, verbose=True):
+    '''Time-evolve mixed Ising Chain using TEBD MPS algorithm. Employs TenPy engine for MPS and MPO representations.
+    '''
+
     print("finite TEBD, real time evolution")
     print("L={L:d}, h={h:.2f}, g={g:.2f}, tmax={tmax:.2f}, dt={dt:.3f}".format(L=L, h=h, g=g, tmax=tmax, dt=dt))
 
@@ -53,4 +56,36 @@ def TEBD_mixed_ising_confined(L, h, g, tmax, dt, verbose=True):
     plt.colorbar().set_label('entropy $S$')
     #filename = 'c_tebd_lightcone_{g:.2f}.pdf'.format(g=g)
     #plt.savefig(filename)
+    return S, sz_ray
+
+def TEBD_DAOE(L, h, g, tmax, dt, verbose=True):
+    print("finite TEBD, real time evolution")
+    print("L={L:d}, h={h:.2f}, g={g:.2f}, tmax={tmax:.2f}, dt={dt:.3f}".format(L=L, h=h, g=g, tmax=tmax, dt=dt))
+
+    model_params = dict(L=L, J=1., h=h, g=g, bc_MPS='finite', conserve=None, verbose=True)
+    M = MixedIsingChain(model_params)
+    product_state = ["up"] * (M.lat.N_sites//2) + ["down"] * (M.lat.N_sites//2) + ["down"] * (M.lat.N_sites%2)
+    psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
+
+    dt_measure = 0.05
+    # tebd.Engine makes 'N_steps' steps of `dt` at once; for second order this is more efficient.
+    tebd_params = {
+        'order': 2,
+        'dt': dt,
+        'N_steps': int(dt_measure / dt + 0.5),
+        'trunc_params': {
+            'chi_max': 50,
+            'svd_min': 1.e-10,
+            'trunc_cut': None
+        },
+        'verbose': verbose,
+    }
+    eng = tebd.Engine(psi, M, tebd_params)
+    S = [psi.entanglement_entropy()];
+    sz_ray = [psi.expectation_value('Sz')]
+
+    for n in range(int(tmax / dt_measure + 0.5)):
+        eng.run()
+        S.append(psi.entanglement_entropy())
+        sz_ray.append(psi.expectation_value('Sz'))
     return S, sz_ray
