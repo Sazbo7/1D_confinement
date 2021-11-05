1118938648b5f3fd2a214fc0e290aa7248990ccb
Joseph Szabo <jszabo94@gmail.com>
2021-11-05 14:37:30 -0400

    added kit ladder and heis ladder

--- exact_diag_confinement.py
+++ exact_diag_confinement.py
@@ -47,33 +47,30 @@ def generate_initial_state(L, initial_state='Haar', seed=None):
     elif initial_state == "InfT":
         init_psi = np.diagonal([2**L]);
 
-    elif initial_state == "Random_product":
-        init_psi =
-
     else:
         print("ERROR: invalid initial state defined \n Possible options: 'Haar', 'pol_[u]', 'inft'.")
 
-    assert(init_psi.shape == (2**L, ))
+    assert(init_psi.shape == (2**L,))
     return init_psi;
 
-def kitaev_ladder(L, J_ray, h_ray, BC='periodic'):
+def kitaev_ladder(L, K_ray, h_ray, BC='periodic'):
 
     if BC == 'periodic':
         BC = 1;
     else:
         BC = 0;
 
-    Jz, Jy, Jx = J_ray[0], J_ray[1], J_ray[2];
+    Kz, Ky, Kx = K_ray[0], K_ray[1], K_ray[2];
     hz, hy, hx = h_ray[0], h_ray[1], h_ray[2];
 
-    H_zz = [[Jz,i,L + i] for i in range(L)] # PBC
-    H_xx_1 = [[Jx,2*i,(2*i+1) % (L-1)] for i in range(L//2 + BC * (L%2))] # PBC
-    H_xx_2 = [[Jx,L + 2*i + 1,L + (2*i+2)%L] for i in range(L//2 + BC * (1 - L%2) - 1*(1 - L%2))] # PBC
-    H_yy_1 = [[Jy,2*i+1,(2*i+2) % (L-1)] for i in range(L//2 + BC * (1 - L%2) - 1*(1 - L%2))] # PBC
-    H_yy_2 = [[Jy,L + 2*i, L + (2*i+1)%L] for i in range(LL//2 + BC * (L%2))] # PBC
-    H_z = [[J_x,i] for i in range(2*L)] # PBC
-    H_y = [[J_x,i] for i in range(2*L)] # PBC
-    H_x = [[J_x,i] for i in range(2*L)] # PBC
+    H_zz = [[Kz,i,L + i] for i in range(L)] # PBC
+    H_xx_1 = [[Kx,2*i,(2*i+1) % (L-1)] for i in range(L//2 + BC * (L%2))] # PBC
+    H_xx_2 = [[Kx,L + 2*i + 1,L + (2*i+2)%L] for i in range(L//2 + BC * (1 - L%2) - 1*(1 - L%2))] # PBC
+    H_yy_1 = [[Ky,2*i+1,(2*i+2) % (L-1)] for i in range(L//2 + BC * (1 - L%2) - 1*(1 - L%2))] # PBC
+    H_yy_2 = [[Ky,L + 2*i, L + (2*i+1)%L] for i in range(LL//2 + BC * (L%2))] # PBC
+    H_z = [[hz,i] for i in range(2*L)] # PBC
+    H_y = [[hy,i] for i in range(2*L)] # PBC
+    H_x = [[hx,i] for i in range(2*L)] # PBC
 
     # define static and dynamics lists
     static=[["zz",H_zz], ["xx",H_xx_1], ["xx",H_xx_2], ["yy",H_yy_1], ["yy",H_yy_2], ["z",H_z], ["y",H_y], ["x",H_x]];
@@ -83,5 +80,34 @@ def kitaev_ladder(L, J_ray, h_ray, BC='periodic'):
     H=hamiltonian(static,dynamic,dtype=np.float64,basis=basis,check_herm=False);
     return H;
 
+def heisenberg_ladder(L, J_ray, h_ray, rung_c = 1.0, BC='periodic'):
 
-def fermion_KitLad(L, J_ray, h_ray, disorder = [0.0,0.0], BC='periodic'):
+    if BC == 'periodic':
+        BC = 1;
+    else:
+        BC = 0;
+
+    Jz, Jy, Jx = J_ray[0], J_ray[1], J_ray[2];
+    hz, hy, hx = h_ray[0], h_ray[1], h_ray[2];
+
+    ### Along Length
+
+    H_zz_r = [[Jz * alpha,i,L + i] for i in range(L)] # PBC
+    H_xx_r = [[Jx * alpha,i,L + i] for i in range(L)] # PBC
+    H_yy_r = [[Jy * alpha,i,L + i] for i in range(L)] # PBC
+
+    H_zz_l = [[Jz ,i,(i+1)%L)] for i in range(L)] # PBC
+    H_xx_l = [[Jx,i,(i+1)%L] for i in range(L)] # PBC
+    H_yy_l = [[Jy,i,(i+1)%L] for i in range(L)] # PBC
+
+    H_z = [[hz,i] for i in range(2*L)] # PBC
+    H_y = [[hy,i] for i in range(2*L)] # PBC
+    H_x = [[hx,i] for i in range(2*L)] # PBC
+
+    # define static and dynamics lists
+    static=[["zz",H_zz_r], ["zz",H_zz_l], ["xx",H_xx_r], ["xx",H_xx_l], ["yy",H_yy_r], ["yy",H_yy_l], ["z",H_z], ["y",H_y], ["x",H_x]];
+    #static=[["zz",H_zz],["z",H_z],["x",H_x]];
+    dynamic=[]
+    basis=spin_basis_1d(L=2*L)
+    H=hamiltonian(static,dynamic,dtype=np.float64,basis=basis,check_herm=False);
+    return H;
